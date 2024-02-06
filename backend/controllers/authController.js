2b0425bd2c9d16a270af21dc99cec0607a9828d4
VishavSingla <vishav9933@gmail.com>
2024-02-06 18:33:30 +0530

    backend auth completed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,188 +4,251 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const signUpController = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
+	try {
+		const { name, email, password } = req.body;
 
-        if (!email || !password || !name) {
-            return res.status(400).json({ error: "Please provide all required fields" });
-        }
+		if (!email || !password || !name) {
+			return res.status(400).json({ error: "Please provide all required fields" });
+		}
 
-        const existingUser = await prisma.User.findUnique({
-            where: {
-                email: email,
-            },
-        });
+		const existingUser = await prisma.user.findUnique({
+			where: {
+				email: email,
+			},
+		});
 
-        if (existingUser) {
-            return res.status(409).json({ error: "User already exists" });
-        }
+		if (existingUser) {
+			return res.status(409).json({ error: "User already exists" });
+		}
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+		const hashedPassword = await bcrypt.hash(password, 10);
 
-        const user = await prisma.User.create({
-            data: { name, email, hashedPassword },
-        });
+		const user = await prisma.user.create({
+			data: { name, email, hashedPassword },
+		});
 
-        const verificationToken = await prisma.VerificationToken.create({
-            data: {
-                identifier: user.email,
-                token: generateVerificationToken(),
-                expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-                userId: user.id,
-            },
-        });
+		const verificationToken = await prisma.verificationToken.create({
+			data: {
+				identifier: user.email,
+				token: generateVerificationToken(),
+				expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+				userId: user.id,
+			},
+		});
 
-        return res.status(200).json({ message: "User registered successfully", user, verificationToken });
-    } catch (err) {
-        return res.status(500).json({ error: "Internal server error", details: err.message });
-    }
+		return res
+			.status(200)
+			.json({ message: "User registered successfully", user, verificationToken });
+	} catch (err) {
+		return res
+			.status(500)
+			.json({ error: "Internal server error", details: err.message });
+	}
 };
 
 const loginController = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+	try {
+		const { email, password } = req.body;
 
-        const existingUser = await prisma.User.findUnique({
-            where: {
-                email,
-            },
-        });
+		const existingUser = await prisma.User.findUnique({
+			where: {
+				email,
+			},
+		});
 
-        if (!existingUser) {
-            return res.status(404).json({ error: "User not found" });
-        }
+		if (!existingUser) {
+			return res.status(404).json({ error: "User not found" });
+		}
 
-        const matched = await bcrypt.compare(password, existingUser.hashedPassword);
+		const matched = await bcrypt.compare(password, existingUser.hashedPassword);
 
-        if (!matched) {
-            return res.status(403).json({ error: "Incorrect password" });
-        }
+		if (!matched) {
+			return res.status(403).json({ error: "Incorrect password" });
+		}
 
-        const accessToken = generateAccessToken({
-            id: existingUser.id,
-        });
+		const accessToken = generateAccessToken({
+			id: existingUser.id,
+		});
 
-        const refreshToken = generateRefreshToken({
-            id: existingUser.id,
-        });
+		const refreshToken = generateRefreshToken({
+			id: existingUser.id,
+		});
 
-        await prisma.User.update({
-            where: {
-                id: existingUser.id,
-            },
-            data: {
-                access_token: accessToken,
-                refresh_token: refreshToken,
-            },
-        });
+		await prisma.User.update({
+			where: {
+				id: existingUser.id,
+			},
+			data: {
+				access_token: accessToken,
+				refresh_token: refreshToken,
+			},
+		});
 
-        const session = await prisma.Session.create({
-            data: {
-                sessionToken: generateSessionToken(),
-                userId: existingUser.id,
-                expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-            },
-        });
+		const session = await prisma.Session.create({
+			data: {
+				sessionToken: generateSessionToken(),
+				userId: existingUser.id,
+				expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+			},
+		});
 
-        return res.status(200).json({ message: "Login successful", accessToken, session });
-    } catch (err) {
-        return res.status(500).json({ error: "Internal server error", details: err.message });
-    }
+		return res
+			.status(200)
+			.json({ message: "Login successful", accessToken, session });
+	} catch (err) {
+		return res
+			.status(500)
+			.json({ error: "Internal server error", details: err.message });
+	}
 };
 
 const refreshAccessTokenController = async (req, res) => {
-    const { userId, refreshToken } = req.body;
+	const { userId, refreshToken } = req.body;
 
-    if (!refreshToken) {
-        return res.status(400).json({ error: "Refresh token is required" });
-    }
+	if (!refreshToken) {
+		return res.status(400).json({ error: "Refresh token is required" });
+	}
 
-    try {
-        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_PRIVATE_KEY);
-        const id = decoded.id;
+	try {
+		const decoded = jwt.verify(
+			refreshToken,
+			process.env.REFRESH_TOKEN_PRIVATE_KEY
+		);
+		const id = decoded.id;
 
-        const user = await prisma.User.findUnique({
-            where: {
-                id,
-            },
-        });
+		const user = await prisma.User.findUnique({
+			where: {
+				id,
+			},
+		});
 
-        if (!user) {
-            return res.status(401).json({ error: "Invalid refresh token" });
-        }
+		if (!user) {
+			return res.status(401).json({ error: "Invalid refresh token" });
+		}
 
-        const newAccessToken = generateAccessToken({ id });
+		const newAccessToken = generateAccessToken({ id });
 
-        return res.status(201).json({ message: "Token refreshed successfully", accessToken: newAccessToken });
-
-    } catch (err) {
-        return res.status(401).json({ error: "Invalid refresh token", details: err.message });
-    }
+		return res.status(201).json({
+			message: "Token refreshed successfully",
+			accessToken: newAccessToken,
+		});
+	} catch (err) {
+		return res
+			.status(401)
+			.json({ error: "Invalid refresh token", details: err.message });
+	}
 };
 
 const generateAccessToken = (data) => {
-    try {
-        const token = jwt.sign(data, process.env.ACCESS_TOKEN_PRIVATE_KEY, {
-            expiresIn: "1d",
-        });
-        return token;
-    } catch (e) {
-        return res.status(500).json({ error: "Failed to generate access token", details: e.message });
-    }
+	try {
+		const token = jwt.sign(data, process.env.ACCESS_TOKEN_PRIVATE_KEY, {
+			expiresIn: "1d",
+		});
+		return token;
+	} catch (e) {
+		return res
+			.status(500)
+			.json({ error: "Failed to generate access token", details: e.message });
+	}
 };
 
 const generateRefreshToken = (data) => {
-    try {
-        const token = jwt.sign(data, process.env.REFRESH_TOKEN_PRIVATE_KEY, {
-            expiresIn: "20d",
-        });
-        return token;
-    } catch (e) {
-        return res.status(500).json({ error: "Failed to generate refresh token", details: e.message });
-    }
+	try {
+		const token = jwt.sign(data, process.env.REFRESH_TOKEN_PRIVATE_KEY, {
+			expiresIn: "20d",
+		});
+		return token;
+	} catch (e) {
+		return res
+			.status(500)
+			.json({ error: "Failed to generate refresh token", details: e.message });
+	}
 };
 
 const logOutController = async (req, res) => {
-    try {
-        console.log(req.User.id)
-        if (!req.user || !req.user.id) {
-            return res.status(400).json({ error: "Invalid user information" });
-        }
+	console.log(req.params.id);
+	console.log("in logout");
+	try {
+		if (!req.params.id) {
+			return res.status(400).json({ error: "session not available" });
+		}
 
-        await prisma.User.update({
-            where: {
-                id: req.User.id,
-            },
-            data: {
-                access_token: null,
-                refresh_token: null,
-            },
-        });
+		// await prisma.User.update({
+		// 	where: {
+		// 		id: req.params.id
+		// 	},
+		// 	data: {
+		// 		access_token: null,
+		// 		refresh_token: null,
+		// 	},
+		// });
 
-        await prisma.Session.deleteMany({
-            where: {
-                userId: req.User.id,
-            },
-        });
-
-        return res.status(200).json({ message: "Logged out successfully" });
-    } catch (e) {
-        return res.status(500).json({ error: "Internal server error", details: e.message });
-    }
+		await prisma.Session.deleteMany({
+			where: {
+				sessionToken: req.params.id,
+			},
+		});
+		console.log("alfter logout");
+		return res.status(200).json({ message: "Logged out successfully" });
+	} catch (e) {
+		return res
+			.status(500)
+			.json({ error: "Internal server error", details: e.message });
+	}
 };
 
 const generateVerificationToken = () => {
-    return Math.random().toString(36).substr(2, 10);
+	return Math.random().toString(36).substr(2, 10);
 };
 
 const generateSessionToken = () => {
-    return Math.random().toString(36).substr(2, 10);
+	return Math.random().toString(36).substr(2, 10);
+};
+
+const checkValidSession = async (req, res) => {
+	const sessionId  = req.params.id;
+	console.log("id1: ",sessionId);
+	try {
+		const thisSession = await prisma.Session.findUnique({
+			where: { sessionToken:  sessionId },
+		});
+		console.log("isession: ",thisSession);
+		if (thisSession) return res.status(200).json({success:"Is Logged In"});
+		return res.status(401).json({error:"Not Logged In"});
+	} catch (e) {
+		return res.status(500).json({ error: "Session Expired", details: e.message });
+	}
+};
+
+const getUserDetails = async (req, res) => {
+	const sessionId  = req.params.id;
+	console.log("Backend Session id ; ",sessionId);
+	console.log("request from front end ");
+	try {
+		const thisSession = await prisma.Session.findUnique({
+			where: { sessionToken:  sessionId },
+		});
+		console.log(thisSession)
+		const thisUser = await prisma.User.findUnique({
+			where : {
+				id:thisSession.userId
+			}
+		});
+		console.log(thisUser)
+		if(thisUser)
+			return res.status(200).send(thisUser);
+		else
+			return res.status(500).json({ error: "No user found", details: e.message });
+	} catch (e) {
+		return res.status(500).json({ error: "Session Expired", details: e.message });
+	}
 };
 
 module.exports = {
-    signUpController,
-    loginController,
-    logOutController,
-    refreshAccessTokenController
+	signUpController,
+	loginController,
+	logOutController,
+	refreshAccessTokenController,
+	checkValidSession,
+	getUserDetails,
 };
--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -1,18 +1,18 @@
 // const cookieParser = require("cookie-parser")
 
-
-const router = require('express').Router();
+const router = require("express").Router();
 // router.use(cookieParser());
 
-const authController = require('../controllers/authController');
+const authController = require("../controllers/authController");
 // const getUserId = require('../controllers/getUserId');
 // const requireUser = require('../middlewares/requireUser');
 
-router.post('/signup', authController.signUpController);
-router.post('/login', authController.loginController);
-router.get('/refresh', authController.refreshAccessTokenController);
-router.post('/logout', authController.logOutController);
+router.post("/signup", authController.signUpController);
+router.post("/login", authController.loginController);
+router.get("/refresh", authController.refreshAccessTokenController);
+router.post("/logout/:id", authController.logOutController);
+router.get("/verifysession/:id", authController.checkValidSession);
+router.get("/getuser/:id", authController.getUserDetails);
 // router.post('/getuser', getUserId);
-
 
 module.exports = router;
