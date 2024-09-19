f370bab0d384fe5abce75aa7d61b11c090c97e67
Mayank Aggarwal <97796657+mayank-0407@users.noreply.github.com>
2024-09-19 17:53:18 +0530

    Update config.js

--- frontend/src/Utils/config.js
+++ frontend/src/Utils/config.js
@@ -1,4 +1,4 @@
 // const backendURL = 'http://localhost:3000/';
-const backendURL = "https://anticorrupto-backend.onrender.com"
+const backendURL = "https://anticorrupto.onrender.com"
 
 export { backendURL };
