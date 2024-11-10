7a788af3f3c1f2fc269a8d28ca5271fb4ad642de
Mayank Aggarwal <97796657+mayank-0407@users.noreply.github.com>
2024-11-10 09:45:17 +0530

    Adding new Backend

--- frontend/src/Utils/config.js
+++ frontend/src/Utils/config.js
@@ -1,5 +1,5 @@
 // const backendURL = "http://localhost:3000";
-const backendURL = "https://anticorrupto.onrender.com"
-
+// const backendURL = "https://anticorrupto.onrender.com"
+const backendURL = "https://antiback-cugagmbkb8gygdgd.centralindia-01.azurewebsites.net"
 
 export { backendURL };
